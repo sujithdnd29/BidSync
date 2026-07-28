2d478baf91f366517550d9b7108e97fbc0261ade
Sujith Reddy <sujithdnd@gmail.com>
2026-07-29 00:45:06 +0530

    Deployment ready Frontend

--- client/src/services/bidService.js
+++ client/src/services/bidService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/bids";
+const API_URL = `${import.meta.env.VITE_API_URL}/bids`;
 
 export const placeBid = async (auctionId, amount) => {
 
